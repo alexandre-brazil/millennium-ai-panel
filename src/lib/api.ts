37ac965b03c53ea56a80d9b61fb2e5a3c5e962da
Alexandre Brasil <alexandre.brazil.official@outlook.com>
2025-09-19 20:35:18 +0100

    add onrender api base url

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,7 +1,7 @@
 // src/lib/api.ts
 import axios from "axios";
 
-const API_URL = process.env.NEXT_PUBLIC_API_URL || "http://localhost:3000/api";
+const API_URL = process.env.NEXT_PUBLIC_API_URL;
 
 // Buscar conversas
 export async function fetchConversations() {

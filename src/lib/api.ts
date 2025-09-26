fdbb157c758cec54a544302481d8370477dd2c9f
Alexandre Brasil <alexandre.brazil.official@outlook.com>
2025-09-26 22:18:23 +0100

    feature development

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,7 +1,7 @@
 // src/lib/api.ts
 import axios from "axios";
 
-const API_URL = process.env.LOCAL_API_URL;
+const API_URL = process.env.NEXT_PUBLIC_API_URL;
 
 // Buscar conversas
 export async function fetchConversations() {
